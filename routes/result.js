5a3b287f8e6f9410d1c6dd8bf77603b894b5b059
cabbageTARO15 <51874845+cabbageTARO15@users.noreply.github.com>
2019-09-24 00:07:23 +0900

    Add files via upload

---
+++ routes/result.js
@@ -0,0 +1,59 @@
+const getID = require('../dlayer/getIdolID');
+const getAppearance = require('../dlayer/getCommonAppearance');
+
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+
+const MYSQL_SETTING = {
+  connectionLimit : 2,
+  host : 'localhost',
+  user : 'idolapi',
+  password : '12345678',
+  database : 'idolsearch'
+};
+
+router.get('/', function(req, res, next) {
+  let idol1Name = req.query.idol1;
+  let idol2Name = req.query.idol2;
+
+  let con = mysql.createPool(MYSQL_SETTING);
+
+  Promise.all([
+    getID(con, [idol1Name]),
+    getID(con, [idol2Name])
+  ]).then(
+    response => {
+      if(response[0].length == 0 || response[1].length == 0){
+        let noFoundName = [];
+        if(response[0].length == 0)noFoundName.push(idol1Name);
+        if(response[1].length == 0)noFoundName.push(idol2Name);
+        res.render('index', {noFoundName : noFoundName});
+        con.end();
+      }else{
+
+        let idol1ID = response[0][0].idol_id;
+        let idol2ID = response[1][0].idol_id;
+        getAppearance(con, idol1ID, idol2ID).then(
+          response => {
+            res.render('result', {togcount : response.length, eventlist : response});
+            con.end();
+          },
+          error => {
+            con.end();
+            throw err;
+          }
+        );
+      }
+    },
+    err => {
+      res.locals.message = err.message;
+      res.locals.error = err;
+      res.status(err.status || 500);
+      res.render('error');
+    }
+  );
+});
+
+module.exports = router;
